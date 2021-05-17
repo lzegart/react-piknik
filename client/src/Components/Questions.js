6fed92085a11350cbf0be76e96e1d5b1a382bf07
LZ <laraezegart@gmail.com>
2021-05-16 21:47:58 -0700

    added components and pages folder with contents

---
+++ client/src/Components/Questions.js
@@ -0,0 +1,18 @@
+import React from "react";
+
+const Questions = () => {
+
+    return (
+        <div id="modal1" className="modal">
+            <div className="modal-content">
+                <h4>Please enter you piknik preferences:</h4>
+                <p>A bunch of text</p>
+            </div>
+            <div className="modal-footer">
+                <a href="#!" class="modal-close waves-effect waves-green btn-flat">plan my piknik!</a>
+            </div>
+        </div>
+    )
+}
+
+export default Questions;
