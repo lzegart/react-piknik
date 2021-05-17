1b845c5c13f6abf64a51f9bc6f575ea859e1ff54
Shelsy Elaine <elainesbarrera@gmail.com>
2021-05-17 16:49:34 -0700

    DELETE: confirm user schema

--- models/confirmModel.js
+++
@@ -1,8 +0,0 @@
-const mongoose = require("mongoose");
-
-const confirmSchema = new mongoose.Schema({
-  token: { type: String, required: true },
-  userID: { type: String, required: true },
-});
-
-module.exports = Confirm = mongoose.model("confirm", confirmSchema);
